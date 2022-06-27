18347eb1c8260aa4b67d7f332c7b43e70addc641
olaide-hok <olaidekareem43@yahoo.com>
2022-06-27 11:41:13 +0100

    Complete Settings Page Functionality.
    Create settings path in core's urls.py.
    
    Create settings views in core's views.py and add restriction decorator.
    
    Rename setting.html to settings.html.
    
    Edit settings.html.
    
    Edit index.html 'Account setting' link to link to settings page.

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
-
+    path('settings', views.settings, name='settings'),
 
 ]
